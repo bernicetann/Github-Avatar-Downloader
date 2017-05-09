80fa5549f6143543a83acef3b8098534874e346b
Bernice Tan <bernice.tann@gmail.com>
2017-05-09 00:40:32 +0000

    Create requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,15 +2,15 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader');
 
+var GITHUB_USER = "bernicetann";
+var GITHUB_TOKEN = "be4a8b00f40b479cd3c37dc5127fe2e5e641ba9c";
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
 
-// getRepoContributors();
+
+getRepoContributors();
