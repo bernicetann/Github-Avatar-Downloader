8c0b6e06ce07f6e747f8af2d19c614a62c22e2ab
Bernice Tan <bernice.tann@gmail.com>
2017-05-09 03:07:57 +0000

    Implement downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,7 @@ var request = require('request');
 
 var GITHUB_USER = "bernicetann";
 var GITHUB_TOKEN = "be4a8b00f40b479cd3c37dc5127fe2e5e641ba9c";
+// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
@@ -35,11 +36,32 @@ function printResponse(error, response, body){
     return
   }
   var parsedResults = JSON.parse(body);
-  console.log(parsedResults);
+  // console.log(parsedResults);
   for (var array in parsedResults) {
-    console.log(parsedResults[array].avatar_url);
+    // console.log(parsedResults[array].avatar_url);
   }
 }
 
 getRepoContributors(process.argv[2],process.argv[3], printResponse);
 
+var fs = require('fs');
+
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Downloading image...');
+       })
+       .on('end', function () {
+          console.log('Download complete.');
+
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars3.githubusercontent.com/u/1615?v=3", "./kvirani.jpg");
