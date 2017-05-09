67a0ab1b5c794aa349e3bc0df431c42be429c37d
Bernice Tan <bernice.tann@gmail.com>
2017-05-09 02:41:58 +0000

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-console.log('Welcome to the Github Avatar Downloader');
+// console.log('Welcome to the Github Avatar Downloader');
 
 var GITHUB_USER = "bernicetann";
 var GITHUB_TOKEN = "be4a8b00f40b479cd3c37dc5127fe2e5e641ba9c";
@@ -10,26 +10,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   request.get({
-    url:  requestURL ,
-    headers:{
+    url: requestURL,
+    headers: {
       'User-Agent':'Lighthouselabs'
     }
-  })
-       .on('error', function (err) {
-          throw err;
-       })
-       .on('response', function (response) {
-          cb(response);
+  }, cb)
+       // .on('error', function (err) {
+       //    throw err;
+       // })
+       // .on('response', function (response) {
+       //    // cb(response);
 
-       })
-       .on('end', function() {
-          console.log('Download complete.');
-       });
+       // })
+       // .on('end', function() {
+       //    console.log('Download complete.');
+       // });
+       // console.log(requestURL);
 }
 
-function printResponse(result){
-  console.log(result)
+
+function printResponse(error, response, body){
+  if(error) {
+    console.log("ERROR", error);
+    return
+  }
+  var parsedResults = JSON.parse(body);
+  console.log(parsedResults);
+  for (var array in parsedResults) {
+    console.log(parsedResults[array].avatar_url);
+  }
 }
 
-getRepoContributors("jquery","jquery",printResponse);
+getRepoContributors(process.argv[2],process.argv[3], printResponse);
 
