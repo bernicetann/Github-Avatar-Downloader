d25692ae71f80304d77fe1c73951fa0e8ca88fdd
Bernice Tan <bernice.tann@gmail.com>
2017-05-09 01:50:14 +0000

    Make the request

--- download_avatars.js
+++ download_avatars.js
@@ -8,9 +8,28 @@ var GITHUB_TOKEN = "be4a8b00f40b479cd3c37dc5127fe2e5e641ba9c";
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+
+  request.get({
+    url:  requestURL ,
+    headers:{
+      'User-Agent':'Lighthouselabs'
+    }
+  })
+       .on('error', function (err) {
+          throw err;
+       })
+       .on('response', function (response) {
+          cb(response);
+
+       })
+       .on('end', function() {
+          console.log('Download complete.');
+       });
 }
 
+function printResponse(result){
+  console.log(result)
+}
 
+getRepoContributors("jquery","jquery",printResponse);
 
-getRepoContributors();
